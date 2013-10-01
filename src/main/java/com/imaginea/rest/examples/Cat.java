bf117833adace66b110b22e688867a77077739f9
Dilip Kumar <dilipsgm@gmail.com>
2013-10-01 17:01:21 +0530

    consumes and produces part done according to review

--- src/main/java/com/imaginea/rest/examples/Cat.java
+++ src/main/java/com/imaginea/rest/examples/Cat.java
@@ -1,11 +1,17 @@
 package com.imaginea.rest.examples;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/Test/Cat")
+//@Consumes({ MediaType.APPLICATION_JSON })
+@Produces({ MediaType.TEXT_HTML})
 public class Cat {
 
 	private String catName;
@@ -18,6 +24,7 @@ public class Cat {
 
 	@GET
 	@Path("dilip/{catname}")
+	@Consumes({ MediaType.APPLICATION_ATOM_XML })
 	public Response getCaDesc(@PathParam("catname") String catName) {
 		String output = catDesc + "<br>sent Data:" + catName;
 		return Response.status(200).entity(output).build();
@@ -25,10 +32,15 @@ public class Cat {
 
 	@GET
 	@Path("/{catname}/{desc}")
-	public Response getCatName(@PathParam("catname") String catN,
-			@PathParam("desc") String desc) {
-		
+	public Response getCatName(@PathParam("catname") String catN, @PathParam("desc") String desc) {
+
 		String output = catName + "<br> sent Parameters:" + catN + "," + desc;
 		return Response.status(200).entity(output).build();
+	}
+
+	@POST
+	@Path("/dilipkumar")
+	public String printDilipKumar() {
+		return "Hi there..! This is Dilip Kumar";
 	}
 }
