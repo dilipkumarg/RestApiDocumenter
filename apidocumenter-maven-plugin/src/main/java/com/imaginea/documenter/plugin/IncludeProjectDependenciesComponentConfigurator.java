33123b3c410adf3ebbfdeaba75c3cbf77379a8cf
Dilip Kumar <dilipsgm@gmail.com>
2013-10-09 18:57:00 +0530

    added markdown formatter for output

--- apidocumenter-maven-plugin/src/main/java/com/imaginea/documenter/plugin/IncludeProjectDependenciesComponentConfigurator.java
+++ apidocumenter-maven-plugin/src/main/java/com/imaginea/documenter/plugin/IncludeProjectDependenciesComponentConfigurator.java
@@ -1,5 +1,13 @@
 package com.imaginea.documenter.plugin;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.maven.plugin.MojoExecution;
 import org.codehaus.classworlds.ClassRealm;
 import org.codehaus.plexus.component.configurator.AbstractComponentConfigurator;
 import org.codehaus.plexus.component.configurator.ComponentConfigurationException;
@@ -8,20 +16,12 @@ import org.codehaus.plexus.component.configurator.converters.composite.ObjectWit
 import org.codehaus.plexus.component.configurator.expression.ExpressionEvaluationException;
 import org.codehaus.plexus.component.configurator.expression.ExpressionEvaluator;
 import org.codehaus.plexus.configuration.PlexusConfiguration;
-
-import java.io.File;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
+import org.codehaus.plexus.util.xml.Xpp3Dom;
 
 /**
  * A custom ComponentConfigurator which adds the project's runtime classpath
  * elements to the
  * 
- * @author Brian Jackson
- * @since Aug 1, 2008 3:04:17 PM
  * 
  * @plexus.component 
  *                   role="org.codehaus.plexus.component.configurator.ComponentConfigurator"
@@ -49,24 +49,28 @@ public class IncludeProjectDependenciesComponentConfigurator extends
 
 	}
 
-	@SuppressWarnings("unchecked")
 	private void addProjectDependenciesToClassRealm(
 			ExpressionEvaluator expressionEvaluator, ClassRealm containerRealm)
 			throws ComponentConfigurationException {
 		List<String> runtimeClasspathElements;
-		/* try { */
-		// noinspection unchecked
-		/*
-		 * runtimeClasspathElements = (List<String>) expressionEvaluator
-		 * .evaluate("${project.runtimeClasspathElements}");
-		 */
-		runtimeClasspathElements = retrieveCustomJars(expressionEvaluator);
-		/*
-		 * } catch (ExpressionEvaluationException e) { throw new
-		 * ComponentConfigurationException(
-		 * "There was a problem evaluating: ${project.runtimeClasspathElements}"
-		 * , e); }
-		 */
+		try {
+			// noinspection unchecked
+			/*
+			 * runtimeClasspathElements = (List<String>) expressionEvaluator
+			 * .evaluate("${project.runtimeClasspathElements}");
+			 */
+			runtimeClasspathElements = retrieveCustomJars(expressionEvaluator);
+			MojoExecution mojoExec = (MojoExecution) expressionEvaluator
+					.evaluate("${mojo}");
+
+			Xpp3Dom attributes = mojoExec.getConfiguration();
+			System.out.println(attributes.getChild("basePath").getValue());
+
+		} catch (ExpressionEvaluationException e) {
+			throw new ComponentConfigurationException(
+					"There was a problem evaluating: ${project.runtimeClasspathElements}",
+					e);
+		}
 
 		// Add the project dependencies to the ClassRealm
 		final URL[] urls = buildURLs(runtimeClasspathElements);
---
+++ apidocumenter-maven-plugin/src/main/java/com/imaginea/documenter/plugin/docgen/Api.java
@@ -0,0 +1,30 @@
+package com.imaginea.documenter.plugin.docgen;
+
+import com.imaginea.documenter.core.model.MethodOperations;
+import com.imaginea.documenter.core.model.MethodResponse;
+
+public class Api {
+	private final String index;
+	private final String basePath;
+	private final MethodResponse api;
+
+	public Api(String index, String basePath, MethodResponse api) {
+		this.api = api;
+		this.index = index;
+		this.basePath = basePath;
+	}
+
+	public String toMarkDown() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("####" + index + ". `" + api.getPath() + "`\n");
+
+		int i = 1;
+		for (MethodOperations operation : api.getOperations()) {
+			Operation operationDoc = new Operation(index + "." + i++,
+					api.getPath(), operation, basePath);
+			sb.append(operationDoc.toMarkDown());
+		}
+
+		return sb.toString();
+	}
+}
